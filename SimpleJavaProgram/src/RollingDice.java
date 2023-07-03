0caa274b6d1dc4a422df819254413c8452d16695
David Moina <davemolina777@gmail.com>
2023-07-02 21:04:04 -0500

    Import Random from java.util.Random and create random number from 1-6

--- SimpleJavaProgram/src/RollingDice.java
+++ SimpleJavaProgram/src/RollingDice.java
@@ -1,8 +1,13 @@
+import java.util.Random;
+
 public class RollingDice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Random randNumber = new Random();
+		int x = randNumber.nextInt(6);
 		System.out.println("David Molina");
+		
 	}
 
 }
