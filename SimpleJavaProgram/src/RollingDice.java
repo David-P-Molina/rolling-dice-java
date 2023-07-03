d167d48f7f26dbe7374ed97b5e73a5bcd5cf733a
David Moina <davemolina777@gmail.com>
2023-07-02 21:06:07 -0500

    Add a 1 to int x to fix range from 0-5 to 1-6

--- SimpleJavaProgram/src/RollingDice.java
+++ SimpleJavaProgram/src/RollingDice.java
@@ -5,9 +5,9 @@ public class RollingDice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Random randNumber = new Random();
-		int x = randNumber.nextInt(6);
+		int x = randNumber.nextInt(6) + 1;
 		System.out.println("David Molina");
-		
+		System.out.println("You rolled a: " + x);
 	}
 
 }
